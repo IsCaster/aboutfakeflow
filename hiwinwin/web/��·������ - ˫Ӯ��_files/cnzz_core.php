5a644c30a9216ea304f0735c6f21b98cb3f77670
caster blue <zxc262403@gmail.com>
2012-08-15 19:41:40 +0800

    update hiwinwin site source

--- hiwinwin/web/��·������ - ˫Ӯ��_files/cnzz_core.php
+++ hiwinwin/web/��·������ - ˫Ӯ��_files/cnzz_core.php
@@ -1,6 +1,7 @@
 (function(){
 try{
 var d=window._CNZZDbridge_2619209.bobject;
+var scheme =  (document.location.protocol=='https:')?'https:':'http:';
 d.callRequest([]);
 d.callScript([]);
     d.createIcon(["<a href='http://www.cnzz.com/stat/website.php?web_id=2619209' target=_blank title='&#31449;&#38271;&#32479;&#35745;'><img border=0 hspace=0 vspace=0 src='http://icon.cnzz.com/pic1.gif'></a>"])}catch(err){
